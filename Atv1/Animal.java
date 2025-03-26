4b2f69a1ba78dc9bffb8fe592c46e10b6d7b99ac
Daniel <dboliveira257@gmail.com>
2025-03-26 20:00:28 -0300

    Add files via upload

---
+++ Atv1/Animal.java
@@ -0,0 +1,13 @@
+class Animal {
+    String nome;
+
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+
+    public void fazerSom() {
+        System.out.println("Som genérico de animal");
+    }
+}
