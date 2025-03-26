81003b6a30a6e23064f59dad8a09e3fed40042f3
Daniel <dboliveira257@gmail.com>
2025-03-26 20:00:13 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Criando um objeto Cachorro
-        Cachorro cachorro = new Cachorro("");
-        cachorro.fazerSom();
-    }
-}
-
